b37fbf9730936d5206763003f3a634a80a7742a1
Michael Tegegn <michaelwalelegn11@gmail.com>
2019-12-08 18:06:36 +0900

    skeleton code to place fragments

--- app/src/main/java/com/example/buddyapp4/ui/main/SectionsPagerAdapter.java
+++ app/src/main/java/com/example/buddyapp4/ui/main/SectionsPagerAdapter.java
@@ -30,7 +30,14 @@ public class SectionsPagerAdapter extends FragmentPagerAdapter {
     public Fragment getItem(int position) {
         // getItem is called to instantiate the fragment for the given page.
         // Return a PlaceholderFragment (defined as a static inner class below).
-        return PlaceholderFragment.newInstance(position + 1);
+        Fragment fragment = null;
+        switch (position) {
+            case 0:
+            case 1:
+            case 2:
+                fragment = PlaceholderFragment.newInstance(position + 1);
+        }
+        return fragment;
     }
 
     @Nullable
