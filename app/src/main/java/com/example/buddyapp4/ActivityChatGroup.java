b1e6e699c352e32b266c7f708bded6602f7df7c9
Michael Tegegn <michaelwalelegn11@gmail.com>
2019-12-10 18:38:46 +0900

    started implemented message handler

--- app/src/main/java/com/example/buddyapp4/ActivityChatGroup.java
+++ app/src/main/java/com/example/buddyapp4/ActivityChatGroup.java
@@ -1,13 +1,29 @@
 package com.example.buddyapp4;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+import java.io.Serializable;
 
 public class ActivityChatGroup extends AppCompatActivity {
+
+    EditText enteredMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat_group);
+
+    }
+
+    public void sendMessage(View v) {
+        enteredMessage = findViewById(R.id.messageEntered);
+
     }
 }
